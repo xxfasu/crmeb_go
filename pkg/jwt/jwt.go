26a41c01d512a4fd10d864747eafcaebefc3ba7a
xiaoxiaofasu <1710247022@qq.com>
2024-12-10 23:07:12 +0800

    init: 添加jwt校验

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"crmeb_go/internal/data/user_data"
 	"crmeb_go/pkg/cache"
+	"encoding/json"
 	"errors"
+	"github.com/gin-gonic/gin"
 	"strings"
 	"time"
 
@@ -86,6 +88,24 @@ func (j *JWT) DeleteToken(token string) error {
 	return j.cache.DelCache(key)
 }
 
+func (j *JWT) GetLoginUser(ctx *gin.Context) (user_data.LoginUserData, error) {
+	var loginUserData user_data.LoginUserData
+	token := getToken(ctx)
+	if len(token) != 0 {
+		key := getTokenKey(token)
+		value, err := j.cache.GetCache(context.Background(), key)
+		if err != nil {
+			return loginUserData, err
+		}
+		err = json.Unmarshal([]byte(value), &loginUserData)
+		if err != nil {
+			return loginUserData, err
+		}
+	}
+
+	return loginUserData, nil
+}
+
 func (j *JWT) VerifyToken(loginUserData user_data.LoginUserData) error {
 	expireTime := loginUserData.ExpireTime
 	currentTime := time.Now().Unix() // 获取当前时间，Unix 时间戳，单位为秒
@@ -102,7 +122,8 @@ func (j *JWT) RefreshToken(loginUserData user_data.LoginUserData) error {
 	loginUserData.LoginTime = time.Now().Unix()
 	loginUserData.ExpireTime = loginUserData.LoginTime + ExpireTime
 	key := getTokenKey(loginUserData.Token)
-	err := j.cache.SetCache(context.Background(), key, loginUserData.Token, time.Duration(loginUserData.ExpireTime)*time.Second)
+	marshal, _ := json.Marshal(loginUserData)
+	err := j.cache.SetCache(context.Background(), key, string(marshal), time.Duration(loginUserData.ExpireTime)*time.Second)
 	if err != nil {
 		return err
 	}
@@ -111,4 +132,12 @@ func (j *JWT) RefreshToken(loginUserData user_data.LoginUserData) error {
 
 func getTokenKey(uuid string) string {
 	return RedisTokenKey + uuid
+}
+
+func getToken(ctx *gin.Context) string {
+	token := ctx.Request.Header.Get("Authori-zation")
+	if token != "" {
+		token = strings.Replace(token, RedisTokenKey, "", -1)
+	}
+	return token
 }
